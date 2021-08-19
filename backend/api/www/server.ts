4e968450cab4346bb60f1248288a58c90ba9fdf8
Lemogodeuc <q.lemogodeuc@gmail.com>
2021-08-19 23:44:46 +0200

    chore(backend): remove server comments

--- backend/api/www/server.ts
+++ backend/api/www/server.ts
@@ -1,9 +1,5 @@
 import app from "../app";
 import { logger } from "../../utils";
-// const socketIO = require("socket.io");
-// const handleSocket = require("../app/socket");
-// const io = socketIO(server);
-// handleSocket(io);
 
 const PORT = process.env.EXPRESS_PORT || 4000;
 
